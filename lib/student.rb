08b3f7d529bb4db85362d3dab77e973e777da997
Tara Sabella <tarasabella@gmail.com>
2018-12-20 22:53:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,19 @@
 class Student
 
+attr_accessor :name, :grade 
+attr_reader :id 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  def initialize(name, student, )
+  class CreateStudent < ActiveRecord:: Migration[5.2]
+ 
+ create_table :student do |t|
+   t.string :name
+   t.string :grade
+   t.integer :id
+
+ end
+  end
+
 end
